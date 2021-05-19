5c1ff880475bdfb9b004d688474d0825e83b476a
João Vitor Silva de Bitencourt <joaov.vbsb@gmail.com>
2021-05-18 21:58:30 -0300

    Rota /Habilidade implementada

--- src/Principal.js
+++ src/Principal.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 import express from 'express';
-import parse from 'node-html-parser'
 
 const app = express();
 const PORTA = process.env.PORT || 8080;
 
 async function geraPokemon() {
-    let resposta2 = await axios.get('https://api.pokemontcg.io/v2/cards'),
-        data2 = resposta2.data;
+    let resposta = await axios.get('https://api.pokemontcg.io/v2/cards'),
+        data = resposta.data;
 
-    return data2;
+    return data;
 }
 
 async function geraPokemon2() {
@@ -26,7 +25,7 @@ async function pokemonEspecifico(URL) {
     return data;
 }
 
-async function retornaPokemon(nome, vetor, res) {
+async function retornaPokemon(nome, vetor) {
     let objPokemon, URL;
     vetor.forEach(element => {
         if (element.name == nome) {
@@ -37,7 +36,7 @@ async function retornaPokemon(nome, vetor, res) {
     return objPokemon;
 }
 
-app.use('/Pokemon2', (req, res) => {
+app.use('/Pokemon', (req, res) => {
     let ListaPokemons = geraPokemon2(),
         queryString = req.query.pokemon,
         vetorPokemon = [],
@@ -47,7 +46,7 @@ app.use('/Pokemon2', (req, res) => {
             vetorPokemon.push(element);
         })
         if (queryString) {
-            Pokemon = retornaPokemon(queryString, vetorPokemon, res);
+            Pokemon = retornaPokemon(queryString, vetorPokemon);
             Pokemon.then(resp => {
                 res.send(resp);
             })
@@ -57,14 +56,51 @@ app.use('/Pokemon2', (req, res) => {
     })
 });
 
-app.use('/Pokemon', (req, res) => {
-    let Pokemon = geraPokemon(req.query.pokemon),
-        a = [];
+app.use('/Habilidades', (req, res) => {
+    let Pokemon = geraPokemon(),
+        a = [],
+        habilidades = [],
+        retorno = [];
     Pokemon.then(resposta => {
+        let ListaPokemons = geraPokemon2(),
+        vetorPokemon = [],
+        queryString = req.query.pokemon;
+
         resposta.data.forEach(element => {
-            a.push(element.name);
+            if(element.abilities){
+                element.abilities.forEach(element => {
+                    habilidades.push(element.name);
+                })
+            }
+            a.push({ name: element.name, abilities: habilidades });
+            habilidades = []
         });
-        res.send(a.join(", "));
+
+        ListaPokemons.then(resposta => {
+            resposta.results.forEach(element => {
+                vetorPokemon.push(element);
+            })
+
+            vetorPokemon.forEach(element => {
+                let nome = element.name;
+                a.forEach(element => {
+                    if(element.name.toLowerCase() == nome) {
+                        retorno.push({name: nome, abilities: element.abilities, data: Date.now()})
+                    }
+                })
+            })
+
+            if (queryString) {
+                console.log(queryString);
+                retorno.forEach(element => {
+                    if(element.name == queryString){
+                        res.json(element);
+                    }
+                })
+            } else {
+                res.json(retorno);
+            } 
+        })
     })
 })
 
